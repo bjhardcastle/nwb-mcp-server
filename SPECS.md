d281c133963922075555c22d263076a1a077febb
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-07-10 18:28:12 -0700

    Fix after testing

--- SPECS.md
+++ SPECS.md
@@ -4,9 +4,9 @@
 - create pl.SQLContext on connection (-> clear memory on exit)
 
 # Tools (expected to perform computation and have side effects)
+- execute SQL query (-> df as json)
+
+# Resources (provide data but shouldn't perform significant computation or have side effects)
 - get list of NWB files
 - get available tables
 - get table schema
-
-# Resources (provide data but shouldn't perform significant computation or have side effects)
-- execute SQL query (-> df as json)
--- src/nwb_mcp_server/server.py
+++ src/nwb_mcp_server/server.py
@@ -1,6 +1,11 @@
-# modified from https://github.com/modelcontextprotocol/servers-archived/blob/main/src/sqlite/src/mcp_server_sqlite/server.py
-
+# /// script
+# dependencies = [
+#   "lazynwb",
+#   "mcp",
+# ]
+# ///
 import asyncio
+import contextlib
 import dataclasses
 import logging
 from typing import AsyncIterator
@@ -21,35 +26,55 @@ logger.info("Starting MCP NWB Server")
 class AppContext:
     db: pl.SQLContext
 
-@asyncio.asynccontextmanager
+@contextlib.asynccontextmanager
 async def server_lifespan(server: FastMCP) -> AsyncIterator[AppContext]:
     """Manage server startup and shutdown lifecycle."""
-    server.settings.nwb_sources = upath.UPath(server.settings.root_dir).glob(server.nwb_file_glob)
+    logger.info("Globbing for NWB files in search directory")
+    server.nwb_sources = upath.UPath(server.search_dir).resolve().glob(server.nwb_file_glob)
     # Initialize the SQL connection to the NWB files
-    sql_context = await asyncio.to_thread(lazynwb.get_sql_context, nwb_sources=server.settings.nwb_sources, full_path=True, disable_progress=True, eager=True)
+    sql_context = await asyncio.to_thread(lazynwb.get_sql_context, nwb_sources=server.nwb_sources, full_path=True, disable_progress=True, eager=True)
     # TODO add server.settings.tables filtering when implemented in lazynwb
     try:
         yield AppContext(db=sql_context)
     finally:
-        # Clean up on shutdown
         await asyncio.to_thread(lazynwb.clear_cache)
 
 server = FastMCP(
     name="nwb-mcp-server",
     lifespan=server_lifespan,
-    # settings:
-    search_dir="C:/Users/ben.hardcastle/github/nwb-mcp-server/data",
-    nwb_file_glob="*.nwb",
-    tables=(),  
 )
 
+server.search_dir="data"
+server.nwb_file_glob="*.nwb"
+server.tables=()
 
-@server.tool()
-async def get_tables(ctx: Context) -> list[str]:
-    """Get the list of available tables in the NWB files."""
-    ctx.info("Fetching available tables from NWB files")
+@server.resource("tables://list")
+async def get_tables() -> list[str]:
+    """Get the list of available tables in the db."""
+    ctx: Context = server.get_context()
+    await ctx.info("Fetching available tables from NWB files")
     return await asyncio.to_thread(ctx.request_context.lifespan_context.db.tables)
 
+@server.tool()
+async def execute_query(query: str) -> bytes:
+    """Run a SQL query against the db and return a dataframe as a JSON string."""
+    ctx: Context = server.get_context()
+    if not query:
+        await ctx.error("No query provided")
+        raise ValueError("Query cannot be empty")
+    await ctx.info(f"Executing query: {query}")
+    df: pl.DataFrame = await asyncio.to_thread(ctx.request_context.lifespan_context.db.execute, query)
+    if df.is_empty():
+        await ctx.warning("Query returned no results")
+        return "[]"
+    await ctx.info(f"Query executed successfully, serializing {len(df)} rows as JSON")
+    if 'obs_intervals' in df.columns:
+        # lists of arrays cause JSON conversion to crash
+        # arrays are ok
+        # TODO report issue to polars
+        # TODO generalize to cast all list[array] columns
+        df = df.cast({'obs_intervals': pl.List(pl.List(pl.Float64))})
+    return df.write_json()
 
 if __name__ == "__main__":
     server.run()
